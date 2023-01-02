e53e215563479f835351568192462d8eac7128da
eigonzalezrojas <eithelgonzalezrojas@gmail.com>
2023-01-01 22:27:54 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,82 @@
+$(function () {
+    // console.log('Jquery its working');
+    
+    /* Carga de datos en la tabla */
+    $('#task-result').hide();
+    fetchTasks();
+    
+    /*Capturamos el elemento con id=search */
+    /*keyup = método de tipeo (cuando el usuario presiona una tecla) */
+    $('#search').keyup(function(e){
+        if ($('#search').val()) {
+            /*seach.val = guarda la tecla presionada */
+            let search = $('#search').val();
+            $.ajax({
+                url: 'task-search.php',
+                type: 'POST',
+                data:{search},
+                success: function(response){
+                    let tasks = JSON.parse(response);
+                    let template = '';
+                    tasks.forEach(task => {
+                        //console.log(task);
+                        template += `<li>
+                            ${task.name}
+                        </li>`
+                    });
+
+                    $('#container').html(template);
+                    $('#task-result').show();
+                }
+            })
+        }
+    });
+
+    /* Interacción con el formulario */
+    $('#task-form').submit(function (e) {
+        const postData={
+            name: $('#name').val(),
+            description: $('#description').val()
+        };
+        
+        $.post('task-add.php', postData, function (response) {
+            /* Recargamos la tabla */
+            fetchTasks();
+
+            /* Resetear valores del formulario */
+            $('#task-form').trigger('reset');
+        });
+        e.preventDefault();
+        
+    })
+
+    /* Mostrar datos en la tabla */
+    function fetchTasks() {
+        /* Insertar los datos en la tabla */
+        $.ajax({
+            url: 'task-list.php',
+            type: 'GET',
+            success: function (response) {
+                /* Se convierte la respuesta (arreglo) en JSON */
+                let tasks = JSON.parse(response);
+                let template = '';
+                tasks.forEach(task=>{
+                    template += `
+                        <tr>
+                            <td>${task.id}</td>
+                            <td>${task.name}</td>
+                            <td>${task.description}</td>
+                            <td><button class="btn btn-danger task-delete">Delete</button></td>
+                        </tr>
+                    `
+                });
+                $('#tasks').html(template);
+            }
+        });
+    }
+
+    /* Botón eliminar de la tabla */
+    $(document).on('click', '.task-delete', function () {
+        console.log('Click');
+    })
+})
---
+++ task-add.php
@@ -0,0 +1,13 @@
+<?php
+    include('database.php');
+    if (isset($_POST['name'])) {
+        $name = $_POST['name'];
+        $description = $_POST['description'];
+        $query = "INSERT into task(name, description) values ('$name', '$description')";
+        $result = mysqli_query($connection, $query);
+        if (!$result) {
+            die('La consulta ha fallado');
+        }
+        echo 'Task Added Successfully';
+    }
+?>
